bfaa1d98029573cd83cfd9aa3254ffa10497608a
Damien <34070001+DefinitelyNotGitHubOfficial@users.noreply.github.com>
2022-08-26 15:00:37 -0500

    initial commit

---
+++ components/HeroHead.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        this is the header
+    </header>
+</template>
+<style lang="scss">
+    header {
+        background-color:#37009c;
+        padding:1rem;
+        color:#e3f600;
+    }
+</style>    
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HeroHead />
+    after
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Hairy Ghost!',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'Hairy Ghost is the Twin Cities Guide to Halloween, Haunted Houses, Movies and Attractions.',
+        name: 'Hairy Ghost',
+        content: 'Hairy Ghost is the Twin Cities Guide to Halloween, Haunted Houses, Movies and Attractions.'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  }
+}
+
+</script>
+<style lang="scss">
+  .red {
+    color: red;
+  }
+</style>
